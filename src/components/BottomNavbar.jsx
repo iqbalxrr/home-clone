399f225b0484ab1884f742874e79064c9bd8491f
iqbalxrr <iamekbal7500@gmail.com>
2025-08-29 15:18:11 +0600

    added

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -5,31 +5,31 @@
 export default function BottomNavbar() {
   return (
     <div className="fixed  bottom-0 left-0 w-full bg-[#1E313E] text-white flex justify-between items-center px-3 ">
-      <div className="flex flex-col items-center absolute top-[-9px] -left-1.5  bg-[#1E313E] rounded-full p-1 pl-1.5 ">
-        <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/games-animation.webp" alt="" className="h-[53px] rounded-full " />
+      <div className="flex flex-col items-center absolute top-[-10px] -left-1.5  bg-[#1E313E] rounded-full p-1 pl-2  ">
+        <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/games-animation.webp" alt="" className="h-[60px] rounded-full " />
       </div>
 
-      <div className="w-full ml-2 flex justify-between items-center text-[11px]">
+      <div className="w-full ml-3 flex justify-between items-center text-[11px]">
         <div className="flex flex-col items-center ">
 
           <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/sport-nav.svg" alt="" />
         </div>
-        <div className="flex flex-col items-center ">
+        <div className="flex flex-col items-center gap-1 pt-1 ">
 
-          <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/sport-nav.svg" alt="" className="h-[17px]" />
-          <span>Sports</span>
+          <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/sport-nav.svg" alt="" className="h-6" />
+          <span className="text-sm">Sports</span>
         </div>
-        <div className="flex flex-col items-center ">
-          <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/inplay-nav.svg" alt="" className="h-[17px]" />
-          <span>In-Play</span>
+        <div className="flex flex-col items-center gap-1 pt-1 ">
+          <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/inplay-nav.svg" alt="" className="h-6" />
+          <span className="text-sm">In-Play</span>
         </div>
-        <div className="flex flex-col items-center bg-gradient-to-b from-[#2F5C78] to-[#243947] py-2 px-4 ">
-          <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/home-nav.svg" alt="" className="h-[17px]" />
-          <span>Home</span>
+        <div className="flex flex-col items-center gap-1 bg-gradient-to-b from-[#2F5C78] to-[#243947] py-2 px-4 ">
+          <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/home-nav.svg" alt="" className="h-6" />
+          <span className="text-sm">Home</span>
         </div>
-        <div className="flex flex-col items-center ">
-          <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/multi-nav.svg" alt="" className="h-[17px]" />
-          <span>Multi...</span>
+        <div className="flex flex-col items-center gap-1 pt-1 ">
+          <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/multi-nav.svg" alt="" className="h-6" />
+          <span className="text-sm">Multi...</span>
         </div>
         <div>
           <div className="flex flex-col items-center relative">
@@ -42,9 +42,9 @@ export default function BottomNavbar() {
               />
             </div>
           </div>
-          <div className="flex flex-col items-center ">
-            <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/account-nav.svg" alt="" className="h-[17px]" />
-            <span className="">Acco...</span>
+          <div className="flex flex-col items-center gap-1 pt-1 ">
+            <img src="https://ik.imagekit.io/bmaxmbpyx/https://skyexcchange.com/assets/images/account-nav.svg" alt="" className="h-6" />
+            <span className="text-sm">Acco...</span>
           </div>
         </div>
 
